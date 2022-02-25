0afde27c394fcba5ce2a072eda03528f4c78ed61
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-25 07:54:17 +0600

    initial commit

---
+++ data-types.js
@@ -0,0 +1,6 @@
+//js is dynamic type variable because we dont have to declare its type and it take decission by own by the right side value;ex-
+// let a = 1;
+// let a = 'this is'
+let a = true;
+
+console.log(typeof a);
